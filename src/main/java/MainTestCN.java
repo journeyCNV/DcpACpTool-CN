6306745e79b3c6431eab9d44333eae3ab8c91245
journeyCNV <50296273+journeyCNV@users.noreply.github.com>
2021-05-07 21:58:24 +0800

    Update MainTestCN.java

--- src/main/java/MainTestCN.java
+++ src/main/java/MainTestCN.java
@@ -6,6 +6,5 @@ public class MainTestCN {
         //UpTestRAR5.unRAR5("C:\\Users\\24725\\Desktop\\界面.rar","C:\\Users\\24725\\Desktop\\Vuerarsn");
         //PressTest.cpZIP("C:\\Users\\24725\\Desktop\\double","C:\\Users\\24725\\Desktop\\double.zip");
         UpTest.unZIP("C:\\Users\\24725\\Desktop\\double.zip","C:\\Users\\24725\\Desktop\\hhh");
-        //UpTest.unZIP("C:\\Users\\24725\\Desktop\\钟嘉骥.zip","C:\\Users\\24725\\Desktop\\hhh");
     }
 }
